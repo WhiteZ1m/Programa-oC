3723144560125747417f1326032b4d6f01941947
Vinicius White <viniciuswhite22@outlook.com>
2025-09-10 12:15:21 +0000

    atualização de condições

--- Olamundo.c
+++ Olamundo.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main (){
+
+    char login[31];
+    char senha[31];
+    int ae2;
+    int chaveHash = 221104;
+    int chaveHashText;
+
+    printf("Insira seu login: \n");
+    scanf("%s", &login);
+    printf("Insira sua senha: \n");
+    scanf("%s", &senha);
+    printf("Insira seu código AE2 do seu app ou SMS: \n");
+    scanf("%d", &ae2);
+    printf("Insira sua chave privada para descriptografia: \n");
+    scanf("%d", &chaveHashText);
+
+     if (chaveHashText == chaveHash){
+
+         printf("Login efetuado com sucesso!\n");
+
+     } else{
+
+      printf("Chave incorreta!\n");
+
+     }
+     
+
+
+}
+
+
 
