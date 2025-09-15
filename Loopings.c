753b2f12269a9519284a7ed6de58c0c3699fe7da
Vinicius White <viniciuswhite22@outlook.com>
2025-09-15 10:42:04 +0000

    Looping while

---
+++ Loopings.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(){
+
+ int i = 1;
+
+ while (i <= 5){ //Enquanto a condição for VERDADEIRA continua a execução, até a condição se tornar falsa e o looping parar.
+
+   printf("O valor de i é: %d\n", i);
+   i++;
+
+ }
+
+
+return 0; 
+
+
+
+
+
+
+}
