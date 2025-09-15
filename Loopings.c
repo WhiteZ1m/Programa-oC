642ee5a58a2ff5be956c81a4de1463caf7ad7d38
Vinicius White <viniciuswhite22@outlook.com>
2025-09-15 12:11:06 +0000

    do while e for

--- Loopings.c
+++ Loopings.c
@@ -2,6 +2,9 @@
 
 
 int main(){
+/*
+
+// Loop While:
 
  int i = 1;
 
@@ -15,6 +18,39 @@ int main(){
 
 return 0; 
 
+*/
+
+/*
+
+// Loop Do While:
+
+
+int i = 1;
+
+do{
+
+ printf("Execução do i: %d\n", i);
+ i++;
+
+}while (i <= 5);
+
+return 0;
+
+*/
+
+/*
+
+// Loop for:
+
+for (int i = 1; i <= 5; i++){
+
+  printf("O valor de i é: %d\n", i);
+
+}
+
+return 0;
+
+*/
 
 
 
