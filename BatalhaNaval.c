4d021aa44f45ca03ebe2a92c2dbc590a9612615d
Vinicius White <viniciuswhite22@outlook.com>
2025-09-16 17:37:58 +0000

    batalhanaval

---
+++ BatalhaNaval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+
+
+int main(){
+
+    char *nomes[] = {"Primeira", "Segunda", "Terceira"}; //Declaração de array do tipo string.
+
+
+   for(int i = 0; i < 3; i++){
+
+   printf("%s \n", nomes[i]);
+
+   }
+
+ printf("\n");
+
+ return 0;
+
+}
